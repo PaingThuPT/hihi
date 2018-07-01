4690466d32d78ee1421d284b4c6dcbfacb71463d
thetkolin <thetkolin2981834@gmail.com>
2018-07-01 21:46:32 +0630

    This is tkl home

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" href="style.css">
+	<title></title>
+</head>
+<body>
+	<div class="center">
+		<h1>Welcome Page</h1>
+	<form action="companylist.php" method="get">
+		<input type="submit" value="Companies list"><br><br>
+</form>
+<form action="add.php" method="post" >
+	<input type="submit" value="Add Company"><br><br>
+</form>
+<form action="search.php" method="post">
+	<input type="search" id="mysrarch" name="search">
+</form>
+</div>
+</body>
+</html>
